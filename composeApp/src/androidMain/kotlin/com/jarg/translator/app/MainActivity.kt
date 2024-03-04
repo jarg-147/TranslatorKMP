06d5d798ffc5e5ad052a69a900165f51009d9d41
jarg-147 <javier@jarg147.com>
2024-03-04 20:31:39 +0100

    feat(android): Add app, move and update main activity

---
+++ composeApp/src/androidMain/kotlin/com/jarg/translator/app/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.jarg.translator.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.jarg.translator.app.navigation.host.TranslateNavHost
+import com.jarg.translator.core.designsystem.theme.TranslatorTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TranslatorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    TranslateNavHost(
+                        navController = navController
+                    )
+                }
+            }
+        }
+    }
+
+}
---
+++ composeApp/src/androidMain/kotlin/com/jarg/translator/app/TranslateApp.kt
@@ -0,0 +1,7 @@
+package com.jarg.translator.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TranslateApp: Application()
