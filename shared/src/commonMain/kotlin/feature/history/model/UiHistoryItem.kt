f69f7684b5c77387302da3b703fac6dd27cd84e9
jarg-147 <javier@jarg147.com>
2024-03-04 20:29:32 +0100

    feat(shared-common): Add models

---
+++ shared/src/commonMain/kotlin/feature/history/model/UiHistoryItem.kt
@@ -0,0 +1,11 @@
+package feature.history.model
+
+import feature.language.model.UiLanguage
+
+data class UiHistoryItem(
+    val id: Long,
+    val fromText: String,
+    val toText: String,
+    val fromLanguage: UiLanguage,
+    val toLanguage: UiLanguage
+)
---
+++ shared/src/commonMain/kotlin/feature/language/model/UiLanguage.kt
@@ -0,0 +1,11 @@
+package feature.language.model
+
+import core.domain.language.model.Language
+
+expect class UiLanguage {
+    val language: Language
+    companion object {
+        fun byCode(langCode: String): UiLanguage
+        val allLanguages: List<UiLanguage>
+    }
+}
